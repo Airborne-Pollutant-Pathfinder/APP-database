47d864306076632f567411b9df32efdf94f8f5da
Arham4 <arhamjsiddiqui@gmail.com>
2023-05-01 16:46:47 -0500

    Add data for all MINTS sensors in Dallas

---
+++ migrations/V3_5__add_all_dallas_mints_sensors.sql
@@ -0,0 +1,14 @@
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(33.010072 -96.643315)', 4326), 1000, 'MINTS', '001e06318c9d');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(32.985936 -96.634575)', 4326), 1000, 'MINTS', '001e06323a12');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(33.190944 -96.730549)', 4326), 1000, 'MINTS', '001e06318cf1');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(32.818112 -96.660107)', 4326), 1000, 'MINTS', '001e06305a12');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(32.781518 -96.867938)', 4326), 1000, 'MINTS', '001e06323a37');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(32.716304 -96.748456)', 4326), 1000, 'MINTS', '001e0636e547');
+INSERT INTO sensor (location, radius_meters, source, source_id)
+VALUES (ST_GeomFromText('POINT(32.679040 -96.757365)', 4326), 1000, 'MINTS', '001e0637372e');
